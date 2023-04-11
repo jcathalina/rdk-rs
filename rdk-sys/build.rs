1834ecea3651eff1f0a0ec0acb5a4b1f389b2f0f
naisuu <julius.cathalina@gmail.com>
2023-04-12 01:35:52 +0200

    Revert build.rs to macOS compatible match

--- rdk-sys/build.rs
+++ rdk-sys/build.rs
@@ -7,32 +7,57 @@ fn main() {
 
     env_logger::init();
 
+    let use_conda = std::env::var("CARGO_FEATURE_DYNAMIC_LINKING_FROM_CONDA").is_ok();
+
     let mut lib_paths = vec![];
     let mut include_paths = vec![];
 
-    // prefer the prefix env var, if not, fall back to the base from the CLI
-    match std::env::var("CONDA_PREFIX") {
-        Ok(prefix) => {
-            include_paths.push(format!("{prefix}/include"));
-            include_paths.push(format!("{prefix}/include/rdkit"));
-            lib_paths.push(format!("{prefix}/lib"));
-        }
-        Err(_) => {
-            let conda = which::which("conda")
-                .map(|p| p.to_str().unwrap().to_string())
-                .unwrap_or_else(|_| panic!("conda not found"));
-            let mut conda = std::process::Command::new(conda);
-            conda.args(&["info", "--base"]);
+    match (std::env::consts::OS, std::env::consts::ARCH, use_conda) {
+        (_, _, true) => {
+            // prefer the prefix env var, if not, fall back to the base from the CLI
+            match std::env::var("CONDA_PREFIX") {
+                Ok(prefix) => {
+                    include_paths.push(format!("{prefix}/include"));
+                    include_paths.push(format!("{prefix}/include/rdkit"));
+                    lib_paths.push(format!("{prefix}/lib"));
+                }
+                Err(_) => {
+                    let conda = which::which("conda")
+                        .map(|p| p.to_str().unwrap().to_string())
+                        .unwrap_or_else(|_| panic!("conda not found"));
+                    let mut conda = std::process::Command::new(conda);
+                    conda.args(&["info", "--base"]);
 
-            let output = conda.output().unwrap();
-            let stdout = String::from_utf8(output.stdout).unwrap();
-            let conda_root = stdout.trim().to_string();
+                    let output = conda.output().unwrap();
+                    let stdout = String::from_utf8(output.stdout).unwrap();
+                    let conda_root = stdout.trim().to_string();
 
-            lib_paths.push(format!("{}/lib", conda_root));
-            include_paths.push(format!("{}/include", conda_root));
-            include_paths.push(format!("{}/include/rdkit", conda_root));
+                    lib_paths.push(format!("{}/lib", conda_root));
+                    include_paths.push(format!("{}/include", conda_root));
+                    include_paths.push(format!("{}/include/rdkit", conda_root));
+                }
+            }
         }
-    }
+        ("macos", "x86_64", _) => {
+            include_paths.push("/usr/local/include".to_string());
+            include_paths.push("/usr/local/include/rdkit".to_string());
+        }
+        ("macos", "aarch64", _) => {
+            include_paths.push("/opt/homebrew/include".to_string());
+            include_paths.push("/opt/homebrew/include/rdkit".to_string());
+            lib_paths.push("/opt/homebrew/lib".to_string())
+        }
+        ("linux", _, _) => {
+            include_paths.push("/usr/local/include".to_string());
+            include_paths.push("/usr/local/include/rdkit".to_string());
+            include_paths.push("/usr/include".to_string());
+            include_paths.push("/usr/include/rdkit".to_string());
+        }
+        (unsupported_os, unsupported_arch, use_conda) => panic!(
+            "sorry, rdkit-sys doesn't support {}/{}/use_conda={} at this time",
+            unsupported_os, unsupported_arch, use_conda
+        ),
+    };
 
     let dir = std::fs::read_dir("src/bindings").unwrap();
     let rust_files = dir
@@ -87,27 +112,27 @@ fn main() {
     for path in lib_paths {
         println!("cargo:rustc-link-search=native={}", path);
     }
+    // println!("cargo:rustc-link-lib=static=c++");
 
     for lib in &[
-        "Catalogs",
-        "ChemReactions",
-        "ChemTransforms",
-        "coordgen",
+        // "Catalogs",
+        // "ChemReactions",
+        // "ChemTransforms",
         "DataStructs",
-        "Depictor",
+        // "Depictor",
         "Descriptors",
         "FileParsers",
         "Fingerprints",
-        "GenericGroups",
+        // "GenericGroups",
         "GraphMol",
         "MolStandardize",
-        "MolTransforms",
-        "PartialCharges",
+        // "MolTransforms",
+        // "PartialCharges",
         "RDGeneral",
-        "RDGeometryLib",
-        "RingDecomposerLib",
+        // "RDGeometryLib",
+        // "RingDecomposerLib",
         "SmilesParse",
-        "Subgraphs",
+        // "Subgraphs",
         "SubstructMatch",
     ] {
         println!("cargo:rustc-link-lib=dylib=RDKit{}", lib);
